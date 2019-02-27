127024333f4f6534cdeb3e38d5c7d6e9c94c261f
Philip Wood <philip.wood@gmail.com>
2019-02-27 09:26:50 +0200

    test scaffold

--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-echo "Not yet implemented!"
-exit -1
+echo "**************************************"
+echo "            TEST RUN"
+echo "**************************************"
+#
+export IMAGE_NAME="philipstaffordwood/kubular-ansible-agent:latest"
+
+export TERM="xterm"
+echo Running in host directory: `pwd`
+docker run \
+    --rm \
+    -v `pwd`:/src \
+    -e TERM=$TERM \
+    $IMAGE_NAME \
+    /src/tests/test_container.sh  
+echo "STATUS CODE: $?"
+echo
+echo "**************************************"
+echo 
